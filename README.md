6b4d01dd75eff6d5cd676f4b9adbf7fbaac9486d
一条小鱼呀！ <62199666+Yuxiaoyu2277@users.noreply.github.com>
2023-11-22 10:25:53 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# project
+Library Management System
+
+Group: 85
+Name: 
+Yu Chun To (12519126),
+Lam Tsz Him (13159488),
+Wong Chun Lung (13246191)
+
+Application link: (https://projectsample381.render.com/)  \\ Need update
+
+********************************************
+# Login
+Through the login interface, each user can access the  information library system by entering their username and password.
+
+Each user has a userID and password;
+[
+	{userid: user1, password: password1},
+	{userid: user2, password: password2},
+	{suerid: user3, password: password3}
+]
+
+After successful login, userid is stored in seesion.
+
+********************************************
+# Logout
+In the home page, each user can log out their account by clicking the logout button.
+
+********************************************
+# Home
+In the home page, it will display "Welcome, (username)" on the right top corner.
+Usescan use the search function, create fuction ,add fucntion on the homepage.
+
+
+
+
+********************************************
+# CRUD service
+- Create
+
+-	A booking document may contain the following attributes with an example: 
+	1)	Booking Name (Shake Shack)
+	2)	Book ID (00000003), book id must be 8 digits
+	3)	Book Author
+	4)	Book Editor
+	5)	Book(s) Remain Availible
+Book Name and Book ID is mandatory, and other attributes are optional.
+
+Create operation is post request, and all information is in body of request.
+
+********************************************
+# CRUD service
+- Read
+- The user can use the serach by the bookid or bookname or bookauthor to list out all the information of the book deatils.
+
+//
+-  There are two options to read and find restaurants list all information or searching by restaurant id.
+
+1) List all information
+	display.ejs will be displayed with all restaurant ID;
+	clicking on restaurant ID, the details will be shown;
+
+2) Searching by restaurant id
+	input id of restaurant you want to find (00000003);
+	id is in the body of post request, and in display.ejs restaurant id will be shown as link;
+	clicking on restaurant ID, the details will be displayed;
+//
+********************************************
+# CRUD service
+- Update
+-	The user can update the bookid information through the details interface.
+-	Among the attribute shown above, BookID cannot be changed. Since BookID is fixed, BookID is searching criteria for updating information. 
+
+//
+-	A restaurant document may contain the following attributes with an example: 
+	1)	Restaurant Name (Shake Shack)
+	2)	Borough (Tung Chung)
+	3)	Street (Tat Tung Road)
+	4)	Restaurant Telephone (29871728), telephone number must be 8 digits
+	5)	Cuisine (American)
+	6)	Description (... Very nice hamburger)
+
+	In example, we updated the borough, street and restaurant contact number.
+//
+********************************************
+# CRUD service
+- Delete
+-	The user can delete the book information through the details interface.
+
+********************************************
+# Restful
+In this project, there are three HTTP request types, post, get and delete.
+- Post 
+	Post request is used for insert.
+	Path URL: /api/item/restaurantID/:restaurantID
+	Test: curl -X POST -H "Content-Type: application/json" --data '{"name": "Taro & Tea", "restaurangID":"00000004"}'localhost:8099/api/item/restaurantID/00000004/name/Taro & Tea
+
+- Get
+	Get request is used for find.
+	Path URL: /api/item/restaurantID/:restaurantID
+	Test: curl -X GET http://localhost:8099/api/item/restaurantID/00000002
+
+- Delete
+	Delete request is used for deletion.
+	Path URL: /api/item/restaurantID/:restaurantID
+	Test: curl -X DELETE localhost:8099/api/item/restaurantID/00000002
+
+For all restful CRUD services, login should be done at first.
+
+
+curl -X POST -H "Content-Type: application/json" --data '{"name": "Taro & Tea", "restaurangID":"00000004"}' http://localhost:8099/api/item/restaurantID/00000004
+
+curl -X GET http://localhost:8099/api/item/restaurantID/00000002
+
+curl -X DELETE http://localhost:8099/api/item/restaurantID/00000002
---
+++ server.js
@@ -0,0 +1,221 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const User = require('./models/User'); // 确保路径正确
+const Book = require('./models/book');
+const app = express();
+
+
+
+app.use(express.json()); // 用于解析 JSON 请求体
+
+mongoose.connect('mongodb+srv://PROJECT:2277236944@cluster0.0cxwsd6.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'your secret key', resave: false, saveUninitialized: false }));
+
+app.set('view engine', 'ejs');
+
+// 登录页面路由
+app.get('/', (req, res) => {
+    res.render('login', { message: null }); // 确保传递 message 变量
+});
+
+// 注册功能的路由
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        // 检查用户是否已存在
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.render('login', { message: 'User already exists', messageType: 'error' });
+        }
+        // 创建新用户
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.render('login', { message: 'User registered successfully', messageType: 'register' });
+    } catch (error) {
+        console.error(error);
+        res.render('login', { message: 'Error occurred during registration', messageType: 'error' });
+    }
+});
+
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.render('login', { message: 'User not found', messageType: 'error' });
+        }
+        if (password === user.password) {
+            req.session.username = user.username; // 保存用户名到会话中
+            res.render('home', { username: user.username, books: [] });
+        } else {
+            res.render('login', { message: 'Invalid username or password', messageType: 'error' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.render('login', { message: 'Error occurred during login', messageType: 'error' });
+    }
+});
+
+app.get('/home', (req, res) => {
+    if (req.session.username) {
+        res.render('home', { username: req.session.username, books: [] });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/search-book', async (req, res) => {
+    try {
+        const { bookId, bookName, author } = req.query;
+        let query = {};
+        
+        if (bookId) query.id = bookId;
+        if (bookName) query.name = bookName;
+        if (author) query.author = author;
+        const books = await Book.find(query);
+        res.render('home', { username: req.session.username, books: books });
+    } catch (error) {
+        console.error(error);
+        res.render('home', { username: req.session.username, books: [] });
+    }
+});
+
+
+app.post('/add-book', async (req, res) => {
+    try {
+        const { id, name, author } = req.body;
+
+        // 检查 id 是否为空
+        if (!id) {
+            return res.render('home', { 
+                username: req.session.username, 
+                books: [],
+                message: 'Book ID is required.'
+            });
+        }
+
+        // 检查是否已存在具有相同 id 的书籍
+        const existingBook = await Book.findOne({ id });
+        if (existingBook) {
+            return res.render('home', { 
+                username: req.session.username, 
+                books: [], 
+                message: 'Book ID already exists.' 
+            });
+        }
+
+        // 创建新书籍
+        const newBook = new Book({ id, name, author });
+        await newBook.save();
+        const books = await Book.find({});
+        res.render('home', { 
+            username: req.session.username, 
+            books: books 
+        });
+    } catch (error) {
+        console.error(error);
+        res.render('home', { 
+            username: req.session.username, 
+            books: [], 
+            message: 'Error occurred during book addition.' 
+        });
+    }
+});
+
+// 删除书籍的路由
+app.post('/delete-book/:id', async (req, res) => {
+    try {
+        await Book.deleteOne({ id: req.params.id });
+        const books = await Book.find({});
+        res.render('home', { username: req.session.username, books: books });
+    } catch (error) {
+        console.error(error);
+        res.render('home', { username: req.session.username, books: [] });
+    }
+});
+
+
+app.post('/update-book/:id', async (req, res) => {
+    try {
+        const bookId = req.params.id;
+        const { name, author } = req.body;
+        await Book.updateOne({ id: bookId }, { name, author });
+
+        const books = await Book.find({});
+        res.render('home', { username: req.session.username, books: books });
+    } catch (error) {
+        console.error(error);
+        res.render('home', { username: req.session.username, books: [] });
+    }
+});
+
+
+
+//创建数据库
+app.post('/api/books', async (req, res) => {
+    try {
+        const newBook = new Book(req.body);
+        const savedBook = await newBook.save();
+        res.status(201).json(savedBook);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+//get
+app.get('/api/books', async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.json(books);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+//update
+app.put('/api/books/:id', async (req, res) => {
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedBook) {
+            return res.status(404).json({ message: "书籍未找到" });
+        }
+        res.json(updatedBook);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+//删除
+app.delete('/api/books/:id', async (req, res) => {
+    try {
+        const deletedBook = await Book.findByIdAndDelete(req.params.id);
+        if (!deletedBook) {
+            return res.status(404).json({ message: "书籍未找到" });
+        }
+        res.json({ message: "书籍已删除" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+//logout
+app.post('/logout', (req, res) => {
+    // Assuming you are using express-session or similar for session management
+    req.session.destroy((err) => {
+        if (err) {
+            console.error(err);
+            // Optionally handle the error, perhaps redirect to an error page
+        } else {
+            res.redirect('/'); // Redirect to the login page
+        }
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
